9e1ac98d76422ba25c4636370883f2781b66e9b6
braianmobiledev <braian7410@gmail.com>
2026-02-11 11:43:17 +0000

    fix for 16kb page size (android)

---
+++ android/jni/build.mk
@@ -0,0 +1,8 @@
+API := 24
+ARMV7 := true
+ARMV7_NEON := true
+ARMV7_BUILD_PATH := android-arm-24
+ARMV7_NEON_BUILD_PATH := android-arm-neon-24
+X86_BUILD_PATH := android-x86-24
+ARM64_BUILD_PATH := android-arm64-24
+X86_64_BUILD_PATH := android-x86_64-24
